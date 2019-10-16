73a76d4050b7d93674b03f21f70f5ec68fdece13
Nicolas Bergeron <bergeron@vaniercollege.qc.ca>
2019-10-16 13:53:39 -0400

    Cannon shoots balls

--- Cannon.java
+++ Cannon.java
@@ -8,10 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Cannon extends Actor
 {
-    /**
-     * Act - do whatever the Cannon wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private final static double CANNON_BALL_VELOCITY = 1500.0;
+    
     public void act() 
     {
         MouseInfo mouse = Greenfoot.getMouseInfo();
@@ -22,6 +20,16 @@ public class Cannon extends Actor
                                                   mouse.getY() - getY());
                                                   
             alignWithVector(cannonToMouse);
+            
+            if (Greenfoot.mouseClicked(null))
+            {
+                cannonToMouse.normalize();
+                cannonToMouse = Vector2D.multiply(cannonToMouse, CANNON_BALL_VELOCITY);
+                
+                CannonBall ball = new CannonBall();
+                ball.setVelocity(cannonToMouse);
+                getWorld().addObject(ball, getX(), getY());
+            }
         }
     }    
     
